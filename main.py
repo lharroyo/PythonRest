46a31ccfcfe8b8dfaca1d3c9a7d4a7c9d161e710
lharroyo <47617813+lharroyo@users.noreply.github.com>
2025-04-20 19:09:13 -0500

    Previous service version

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from sqlalchemy import create_engine
 from models.base import Base
-from models.DepartmentModel import Department
-from models.JobModel import Job
-from models.HiredEmployeesModel import HiredEmployee
 from services.DepartmentService import DepartmentService
 
 if __name__ == "__main__":
-    engine = create_engine('sqlite:///employees.db')
+    engine = create_engine('sqlite:///data/employees.db')
     Base.metadata.create_all(engine)
-
-    service = DepartmentService()
+    service = DepartmentService(engine)
     departments = service.get_all_departments()
     print("Lista de departamentos:")
     for dept in departments:
         print(f"- {dept.name}")
+
     service.close()
