b5ac17ba36c214e30e203646fbe3c809fde678ae
lharroyo <47617813+lharroyo@users.noreply.github.com>
2025-04-21 19:22:46 -0500

    Fixes

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,13 @@
 FROM python:3.12-slim
 LABEL authors="lharroyo"
 
-# Establecer directorio de trabajo
 WORKDIR /app
 
-# Copiar e instalar dependencias
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copiar el resto de los archivos del proyecto
 COPY . .
 
-# Exponer el puerto en el que correrá Flask
 EXPOSE 8080
 
-# Comando de inicio
 CMD ["python", "main.py"]
--- dashexample.py
+++ dashexample.py
@@ -6,27 +6,24 @@ import requests
 app = dash.Dash(__name__)
 
 def fetch_hires_data():
-    response = requests.get("http://127.0.0.1:5000/hiredemployees/byquarter")
+    response = requests.get("https://pythonemployees.azurewebsites.net/hiredemployees/byquarter")
     if response.status_code == 200:
         return response.json()
     else:
         return {"message": "Error fetching data", "results": []}
 
 def fetch_average_hires_data():
-    response = requests.get("http://127.0.0.1:5000/hiredemployees/byaveragehires")
+    response = requests.get("https://pythonemployees.azurewebsites.net/hiredemployees/byaveragehires")
     if response.status_code == 200:
         return response.json()
     else:
         return {"message": "Error fetching data", "results": []}
 
-# Obtener datos de contrataciones
 hires_data = fetch_hires_data()
 hires_results = hires_data.get("results", [])
 
-# Convertir los resultados a un DataFrame
 df_hires = pd.DataFrame(hires_results)
 
-# Pivotar el DataFrame para obtener las columnas Q1, Q2, Q3, Q4
 df_hires_pivot = df_hires.pivot_table(
     index=['department', 'job'],
     columns='quarter',
@@ -35,18 +32,14 @@ df_hires_pivot = df_hires.pivot_table(
     fill_value=0
 ).reset_index()
 
-# Renombrar las columnas para que sean más descriptivas
-df_hires_pivot.columns.name = None  # Eliminar el nombre de la columna
+df_hires_pivot.columns.name = None
 df_hires_pivot = df_hires_pivot.rename(columns={1: 'Q1', 2: 'Q2', 3: 'Q3', 4: 'Q4'})
 
-# Obtener datos de departamentos con contrataciones por encima del promedio
 average_hires_data = fetch_average_hires_data()
 average_hires_results = average_hires_data.get("results", [])
 
-# Convertir los resultados a un DataFrame
 df_average_hires = pd.DataFrame(average_hires_results)
 
-# Crear el layout de la aplicación
 app.layout = html.Div([
     html.H1("Hires Data"),
     dcc.Tabs([
